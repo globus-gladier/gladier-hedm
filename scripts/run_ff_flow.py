46400175eaa99478f8b8a083dcc3e1c362ffb13d
Rafael Vescovi <ravescovi@gmail.com>
2021-08-26 10:54:59 -0500

    minor changes

--- scripts/run_ff_flow.py
+++ scripts/run_ff_flow.py
@@ -4,20 +4,29 @@ import time, json, sys, os, copy
 import datetime
 import argparse
 
-if __name__ == '__main__':
+
+def arg_parse():
 	parser = argparse.ArgumentParser(description='''MIDAS_FF, Parameter file must be in the same folder as the desired output folder(SeedFolder)''', formatter_class=RawTextHelpFormatter)
 	parser.add_argument('-nCPUs',    type=int, required=True, help='Number of CPU cores per node to use')
 	parser.add_argument('-numNodes',    type=int, required=True, help='Number of nodes to use')
 	parser.add_argument('-startLayerNr',type=int,required=True,help='Start Layer Number')
 	parser.add_argument('-endLayerNr',type=int,required=True,help='End Layer Number')
 	parser.add_argument('-paramFile', type=str, required=True, help='ParameterFileName')
-	parser.add_argument('-deployment', type=str, required=True, help=f'Locations to run. Available: {list(deployment_map.keys())}')
-	args, unparsed = parser.parse_known_args()
+	parser.add_argument('--deployment','-d', default=None, type=str, help=f'Locations to run. Available: {list(deployment_map.keys())}')
+	return parser.parse_known_args()
+
+if __name__ == '__main__':
+
+	args, unparsed = arg_parse
+
+
 	paramFN = args.paramFile
 	startLayerNr = int(args.startLayerNr)
 	endLayerNr = int(args.endLayerNr)
 	numProcs = int(args.nCPUs)
 	numBlocks = int(args.numNodes)
+
+
 	thisT = datetime.datetime.now()
 	tod = datetime.date.today()
 	timePath = str(tod.year) + '_' + str(tod.month).zfill(2) + '_' + str(tod.day).zfill(2) + '_' + str(thisT.hour).zfill(2) + '_' + str(thisT.minute).zfill(2) + '_' + str(thisT.second).zfill(2)
@@ -38,7 +47,7 @@ if __name__ == '__main__':
 	nFrames = endNr - startNr + 1
 	sourcePath = rawDir
 
-	depl = deployment_map.get(deployment)
+	depl = deployment_map.get(args.deployment)
 	depl_input = depl.get_input()
 	
 	## Set up paths
